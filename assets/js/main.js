e9cf2bab9c3782adb407d69b9b5fef8a55b15f0b
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-22 17:03:47 +0100

    fix alert text

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,12 +77,12 @@ document.querySelector('.lucky_button').addEventListener('click', function () {
         //Stampo con un Alert
         alert("HAI VINTO " + user_number + " a " + pc_number)
     } else if (user_number < pc_number) {
-        console.log("Ha vinto l'IA!");
+        console.log("Ha vinto l'IA! ");
         //Stampo con un Alert
         alert("L' IA HANNO VINTO " + pc_number + " a " + user_number)
     } else {
-        console.log("Pareggio!");
+        console.log("Pareggio! ");
         //Stampo con un Alert
-        alert("PAREGGIO" + pc_number + " a " + user_number)
+        alert("PAREGGIO " + pc_number + " a " + user_number)
     }
 })
